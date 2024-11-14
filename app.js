f5eb51b193b0c8cd9b8946b2b6917babf6652a33
miranda-ale <alessandro.siqueira@outlook.com>
2024-11-14 20:49:33 -0300

    corrige declaração da variável chute na função limparCampo

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ function gerarNumeroAleatorio() {
 }
 
 function limparCampo() {
-    chute = document.querySelector('input');
+    let chute = document.querySelector('input');
     chute.value = '';
 }
 
